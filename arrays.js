fd4bb2b76b815496987646f8081280f6b124a976
Travis Parks <travisparks0788@gmail.com>
2022-11-09 18:58:38 -0500

    Add three new array methods

--- arrays.js
+++ arrays.js
@@ -2,3 +2,14 @@ var places = ["Scotland", "New Zealand", "Japan", "Bhutan"]
 var birthDates = [1988, 1990, 1984, 1956]
 var wullyBooleans = [3 < 9, 10 > 5, 6 > 2, 1 > 0]
 
+//This will remove the last element "Bhutan" from the array 
+places.pop()
+console.log(places)
+
+//This will add 1932 as the last birthDate in the array
+birthDates.push(1932)
+console.log(birthDates)
+
+//This will remove the first boolean in the array
+wullyBooleans.shift()
+console.log(wullyBooleans)
